108065997a10df34c7c691b292c8ea9178494766
pybites <pybites@projects.bobbelderbos.com>
2017-05-12 02:23:32 +0200

    added search

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import calendar
 from datetime import datetime, date, timedelta
 
 from flask import Flask, render_template, abort, redirect, request
+from sqlalchemy import asc
 
 from model import Birthday, app, db, THIS_YEAR
 
-UPCOMING = 'Upcoming'
-MONTHS = [UPCOMING] + list(calendar.month_name)[1:]
+DEFAULT_FIRST_TAB = 'Upcoming'
+TABS = [DEFAULT_FIRST_TAB] + [m[:3] for m in 
+                              list(calendar.month_name)[1:]]
 UPCOMING_DAYS = 14
 
 
@@ -19,8 +21,8 @@ def upcoming():
 
     return render_template("index.html", 
                            data=bdays, 
-                           month=UPCOMING,
-                           months=MONTHS)
+                           active_tab=DEFAULT_FIRST_TAB,
+                           tabs=TABS)
 
 
 @app.route('/<int:month>')
@@ -29,8 +31,6 @@ def bdays_month(month):
         print('{} is not a valid month'.format(month))
         abort(400)
 
-    month_name = calendar.month_name[month]
-
     # how to get first last dt of month?
     # http://stackoverflow.com/questions/36155332/how-to-get-the-first-day-and-last-day-of-current-month-in-python
     _, num_days = calendar.monthrange(THIS_YEAR, month)
@@ -38,10 +38,31 @@ def bdays_month(month):
     end = date(THIS_YEAR, month, num_days)
 
     bdays = Birthday.query.filter(Birthday.bday <= end).filter(Birthday.bday >= start)
+
+    month_name = calendar.month_name[month][:3]
+
     return render_template("index.html", 
                            data=bdays, 
-                           month=month_name,
-                           months=MONTHS)
+                           active_tab=month_name,
+                           tabs=TABS)
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    name = request.args.get('name')
+    if not name.isalpha():
+        print('Not isalpha string')
+        return redirect(request.referrer)
+    bdays = Birthday.query.filter(Birthday.name.like("%{}%".format(name))).order_by(asc(Birthday.bday)).all()
+
+    title = 'Search'
+    tabs = [title] + TABS[1:]
+
+    return render_template("index.html", 
+                           data=bdays,
+                           active_tab=title,
+                           tabs=tabs)
+    
 
 
 @app.route('/notify/<int:person>')
