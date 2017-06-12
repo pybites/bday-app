0e87f24b2431decb2fea8a90da5f27c7f0b94fd1
pybites <pybites@projects.bobbelderbos.com>
2017-06-12 11:51:16 +0200

    add login pep8 app.py

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 import calendar
 from datetime import datetime, date, timedelta
-import os
+from functools import wraps
 import re
 import time
 
-from flask import Flask, render_template, abort, redirect
-from flask import request, url_for, send_file
+from flask import abort, flash, redirect, render_template
+from flask import request, url_for, send_file, session
 from sqlalchemy import asc
 import requests
 
+from env import LOGIN, PASSWORD
 from model import Birthday, app, db, THIS_YEAR
 from notify import BASE_URL
 from sms import send_sms
 from text_on_image import download_url, create_img_with_text, CARDS
 
 DEFAULT_FIRST_TAB = 'Upcoming'
-TABS = [DEFAULT_FIRST_TAB] + [m[:3] for m in 
+TABS = [DEFAULT_FIRST_TAB] + [m[:3] for m in
                               list(calendar.month_name)[1:]]
 UPCOMING_DAYS = 14
 TWILIO_SMS_CHAR_LIMIT = 160
 
+
+def login_required(test):
+    '''From RealPython Flask course'''
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# other helpers
 
 def _get_current_date():
     return date.today().replace(year=THIS_YEAR)
@@ -40,7 +55,30 @@ def _is_valid_url(url):
         return False
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = None
+    status_code = 200
+    if request.method == 'POST':
+        user = request.form.get('username')
+        password = request.form.get('password')
+        if user != LOGIN or password != PASSWORD:
+            flash('Invalid credentials')
+            status_code = 401
+        else:
+            session['logged_in'] = user
+            return redirect(url_for('index'))
+    return render_template('login.html', user=user), status_code
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+
 @app.route('/')
+@login_required
 def index():
     start = datetime.now() - timedelta(days=1)
     end = start + timedelta(days=UPCOMING_DAYS)
@@ -57,9 +95,10 @@ def index():
 
 
 @app.route('/<int:month>')
+@login_required
 def bdays_month(month):
     if month not in range(1, 13):
-        abort(400 , 'Not a valid month')
+        abort(400, 'Not a valid month')
 
     # SO questions/36155332
     _, num_days = calendar.monthrange(THIS_YEAR, month)
@@ -73,34 +112,38 @@ def bdays_month(month):
 
     month_name = calendar.month_name[month][:3]
 
-    return render_template("index.html", 
-                           data=bdays, 
+    return render_template("index.html",
+                           data=bdays,
                            now=now,
                            active_tab=month_name,
                            tabs=TABS)
 
 
 @app.route('/search', methods=['GET'])
+@login_required
 def search():
     name = request.args.get('name')
     if not name.isalpha():
         print('Not isalpha string')
         return redirect(request.referrer)
 
-    bdays = Birthday.query.filter(Birthday.name.like("%{}%".format(name))).order_by(asc(Birthday.bday)).all()
+    bdays = (Birthday.query
+             .filter(Birthday.name.like("%{}%".format(name)))
+             .order_by(asc(Birthday.bday)).all())
 
     now = _get_current_date()
     title = 'Search'
     tabs = [title] + TABS[1:]
 
-    return render_template("index.html", 
+    return render_template("index.html",
                            data=bdays,
                            now=now,
                            active_tab=title,
                            tabs=tabs)
-    
+
 
 @app.route('/friends/<int:friendid>', methods=['GET', 'POST'])
+@login_required
 def update(friendid):
     friend = _get_friend_or_abort(friendid)
 
@@ -111,11 +154,12 @@ def update(friendid):
         if not phone:
             error = 'Please fill in phone number'
         elif not re.match('^\+\d+$', phone):
-            error = 'Please fill in phone number as +digits (e.g. +34666555444)'
+            error = 'Please fill in phone as +digits (e.g. +34666555444)'
 
-        equal_phones_in_db = Birthday.query.filter(Birthday.phone == phone).first()
+        equal_phones_in_db = Birthday.query.filter(
+                             Birthday.phone == phone).first()
         if equal_phones_in_db:
-            error = '{} already in database, please pick another number'.format(phone)
+            error = '{} already in database'.format(phone)
 
         if not error:
             if friend.phone != phone:
@@ -127,8 +171,8 @@ def update(friendid):
     return render_template('update.html', friend=friend, error=error)
 
 
-
 @app.route('/birthday/<int:friendid>', methods=['GET'])
+@login_required
 def send_card(friendid):
     friend = _get_friend_or_abort(friendid)
     today = datetime.now()
@@ -144,6 +188,7 @@ def send_card(friendid):
 
 
 @app.route('/birthday/<int:friendid>/confirm', methods=['POST'])
+@login_required
 def confirm_card(friendid):
     if request.method != 'POST':
         abort(400, 'Post submit route only')
@@ -164,12 +209,12 @@ def confirm_card(friendid):
         if not msg:
             error = 'Please provide a message'
         elif len(msg) > TWILIO_SMS_CHAR_LIMIT:
-            error = 'Please limit your message to {} chars'.format(TWILIO_SMS_CHAR_LIMIT)
+            error = 'Max message size: {} chars'.format(TWILIO_SMS_CHAR_LIMIT)
         elif url and not _is_valid_url(url):
             error = 'URL not valid or reachable'
 
         if error:
-            return render_template('card.html', 
+            return render_template('card.html',
                                    friend=friend,
                                    msg=msg,
                                    url=url,
@@ -184,12 +229,12 @@ def confirm_card(friendid):
             print(url)
 
         return render_template('send.html',
-                                friend=friend,
-                                msg=msg,
-                                url=url,
-                                utstamp=utstamp)
+                               friend=friend,
+                               msg=msg,
+                               url=url,
+                               utstamp=utstamp)
 
-    else: 
+    else:
         # good to send!
 
         if url:
@@ -215,10 +260,8 @@ def confirm_card(friendid):
 
 @app.route("/cards/<name>.png")
 def get_card(name):
-    # https://stackoverflow.com/questions/21714653/flask-css-not-updating
-    # doing this here does not work, do it in template img tag
     img = 'cards/' + name + '.png'
-    return send_file(img, mimetype='image/png')
+    return send_file(img, mimetype='image/png')  # caching! add ?str in img tag
 
 
 if __name__ == "__main__":
